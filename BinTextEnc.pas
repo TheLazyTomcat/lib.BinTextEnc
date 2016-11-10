d5247624e0406d988538427129ff183b09128831
František Milt <frantisek.milt@gmail.com>
2016-11-10 16:40:16 +0100

    Added links to dependencies

--- BinTextEnc.pas
+++ BinTextEnc.pas
@@ -21,6 +21,9 @@
     - Base85 encoding is by-default using Z85 alphabet with undescore ("_", #95)
       as an all-zero compression letter.
 
+  Dependencies:
+    AuxTypes - github.com/ncs-sniper/Lib.AuxTypes
+
 ===============================================================================}
 unit BinTextEnc;
 
